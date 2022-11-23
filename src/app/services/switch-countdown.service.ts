15be589f36e96e44e13180da7ca126d9d34c6d4e
Alexey Udodov <weberdster@gmail.com>
2022-11-23 22:41:14 +0400

    added worktime manipulation

--- src/app/services/switch-countdown.service.ts
+++ src/app/services/switch-countdown.service.ts
@@ -4,6 +4,7 @@ import {RestCountdownService} from "./rest-countdown.service";
 import {SoundService} from "./sound.service";
 import {Injectable} from "@angular/core";
 import {WorkTimeStatService} from "./work-time-stat.service";
+import {WorkTimeModel} from "../models/work-time.model";
 
 @Injectable()
 export class CountdownFactory {
@@ -16,9 +17,13 @@ export class CountdownFactory {
     let newCountdown = new WorkCountdownService()
 
     if (countdown instanceof WorkCountdownService) {
+      const start = new Date()
+      const end = new Date()
+      start.setTime(start.valueOf() - countdown.seconds * 1000)
+
+      this.worktimeStats.unshift(new WorkTimeModel(start, end))
       newCountdown = new RestCountdownService()
       newCountdown.seconds = Math.floor(countdown.seconds / delimiter)
-      this.worktimeStats.totalSeconds += countdown.seconds
       newCountdown.unpause()
     } else {
       this.soundService.playGet2Work()
--- src/app/services/work-time-stat.service.ts
+++ src/app/services/work-time-stat.service.ts
@@ -1,6 +1,40 @@
 import {Injectable} from "@angular/core";
+import {WorkTimeModel} from "../models/work-time.model";
+
+const LOCAL_STORAGE_KEY = 'advanced-pomodoro-worktimes';
 
 @Injectable()
 export class WorkTimeStatService {
-  totalSeconds = 0;
+  private _worktimes: WorkTimeModel[] = [];
+
+  get totalSeconds(): number {
+    return this.worktimes.reduce((seconds, worktime) => seconds + worktime.seconds, 0)
+  }
+
+  get worktimes() {
+    this._worktimes = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '[]')
+    this._worktimes = this._worktimes.map(worktime => {
+      return new WorkTimeModel(new Date(worktime.start), new Date(worktime.end))
+    })
+    return this._worktimes
+  }
+
+  private save() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this._worktimes))
+  }
+
+  unshift(value: WorkTimeModel) {
+    this._worktimes.unshift(value)
+    this.save()
+  }
+
+  remove(index: number) {
+    this._worktimes.splice(index, 1)
+    this.save()
+  }
+
+  removeOlder(index: number) {
+    this._worktimes.splice(index)
+    this.save()
+  }
 }
