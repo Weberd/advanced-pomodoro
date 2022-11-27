12b7cddf8e4a47697011b1a101a0c917fe8801a5
Alexey Udodov <weberdster@gmail.com>
2022-11-28 00:28:23 +0400

    updated time parts in table + fixed for delimiter

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {timer} from "rxjs";
+import {fromEvent, Subject, takeUntil, timer} from "rxjs";
 import {CountdownServiceInterface} from "../services/countdown.service.Interface";
 import {WorkCountdownService} from "../services/work-countdown.service";
 import {CountdownFactory} from "../services/switch-countdown.service";
@@ -24,20 +24,53 @@ export class CountdownComponent implements OnInit {
   ) { }
 
   public countdownService: CountdownServiceInterface = new WorkCountdownService();
-  public delimeter = 4;
+  public delimiter = 4;
+  private _unsubscribeAll: Subject<any> = new Subject();
 
   ngOnInit(): void {
-    timer(0, 1000).subscribe(() => {
+    timer(0, 1000)
+    .pipe(takeUntil(this._unsubscribeAll))
+    .subscribe(() => {
       this.countdownService.progress();
 
       if (this.countdownService.finished())
-        this.switchCountdown()
+        this.switchCountdown(true)
 
       this.titleService.setTitle(`${this.countdownService.title} ${this.hmsPipe.transform(this.countdownService.seconds)}`)
     });
+
+    fromEvent<KeyboardEvent>(document, 'keyup')
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe((event: KeyboardEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+        let key = event.code;
+
+        if (key === 'Space') {
+          this.countdownService.switchPause()
+        }
+
+        if (key === 'KeyF') {
+          this.switchCountdown(false)
+        }
+      })
   }
 
-  switchCountdown() {
-    this.countdownService = this.countdownFactory.switch(this.countdownService, this.delimeter)
+  localeDateTime(date: Date) {
+    return date.toLocaleTimeString()
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next('');
+    this._unsubscribeAll.complete();
+  }
+
+  switchCountdown(playSound: boolean) {
+    this.countdownService = this.countdownFactory.switch(
+      this.countdownService,
+      this.delimiter,
+      playSound
+    )
   }
 }
